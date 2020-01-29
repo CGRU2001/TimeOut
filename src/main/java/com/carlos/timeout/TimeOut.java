1ee0a0c4b17e7bc517206be1dbb58bd8e374e352
Carlos <carlosguillermoru@outlook.com>
2020-01-29 21:28:06 +0100

    Numbers Static method created

--- src/main/java/com/carlos/timeout/TimeOut.java
+++ src/main/java/com/carlos/timeout/TimeOut.java
@@ -10,11 +10,25 @@ package com.carlos.timeout;
  * @author carlos
  */
 public class TimeOut {
-    public static void symbols(String symbol, int repeats){
+
+    public static void symbols(String symbol, int repeats) {
         try {
             System.out.print("-*-");
             for (int i = repeats; i > 0; i--) {
                 System.out.print(symbol);
+                Thread.sleep(1000);
+            }
+            System.out.println("");//Añade un salto de línea
+        } catch (InterruptedException ie) {
+            System.out.println("Congrats! You've found a bug!");
+        }
+    }
+
+    public static void numbers(int numbers) {
+        try {
+            System.out.print("-*-");
+            for (int i = numbers - 1; i > 0; i--) {
+                System.out.print(numbers);
                 Thread.sleep(1000);
             }
             System.out.println("");//Añade un salto de línea
